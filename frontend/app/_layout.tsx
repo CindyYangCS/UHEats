fe2ca54ee08e21fd4cda1ff0940c33275f3fafa8
crms-n <tbgoble@icloud.com>
2026-09-02 18:26:43 -0500

    Calculate route added to frontend.

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack />;
+  return (<Stack >
+    <Stack.Screen name="index" options={{title: 'Main'}} />
+    <Stack.Screen name="calculate" options={{title: 'Calculate'}} />
+
+    </Stack>
+    );
 }
--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,9 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import {Link} from "expo-router";
 
 export default function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ fontSize: 24, fontWeight: "bold" }}>UHEats</Text>
+
+      <Link href="/calculate" style={styles.button}>
+        Go to Calculate Screen
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#333",
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    fontSize: 18,
+    color: "#fff",
+    textDecorationLine: "underline",
+    backgroundColor: "#ff0000",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    textAlign: "center",
+  },
+});
