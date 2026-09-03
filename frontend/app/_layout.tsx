93434a8509c630e37f2445e64b3161f0df7e4d8a
crms-n <tbgoble@icloud.com>
2026-09-03 14:27:58 -0500

    fixed indentation

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return (<Stack >
-    <Stack.Screen name="index" options={{title: 'Main'}} />
-    <Stack.Screen name="calculate" options={{title: 'Calculate'}} />
-
+  return (
+    <Stack >
+      <Stack.Screen name="index" options={{title: 'Main'}} />
+      <Stack.Screen name="calculate" options={{title: 'Calculate'}} />
     </Stack>
     );
 }
